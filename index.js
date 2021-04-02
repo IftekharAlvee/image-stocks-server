2f9a9f6f659ee126815f76736c07cc336605e725
Iftekhar Alvee <alveee1998@gmail.com>
2021-04-02 18:25:44 +0600

    hh

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-client.connect(err => {
+client.connect(error => {
   const imageCollection = client.db("imageStock").collection("images");
   const ordersCollection = client.db("imageStock").collection("orders");
 
@@ -67,14 +67,12 @@ client.connect(err => {
     
   });
 
-  app.get('/', (req, res) => {
-    res.send('Heroku deployrd')
-  })
-
   console.log("Database connected");
 });
 
 
-
+app.get('/', (req, res) => {
+  res.send('Heroku deployrd')
+})
 
 app.listen(port);
